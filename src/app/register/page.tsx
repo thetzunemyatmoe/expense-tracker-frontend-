50e07f42c95270152bfac2165083d754134e9600
Thet Myat Moe <tzm295@student.bham.ac.uk>
2026-01-13 19:22:26 +0000

    Register form UI

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,22 +1,99 @@
 "use client"
-import React from 'react'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
 import { z } from 'zod'
 
-export const registerSchema = z
+export const registerFormSchema = z
   .object({
     email: z.email("Enter a valid email"),
     password: z.string().min(8, "Password must be at least 8 characters").max(50),
-    confirmPassword: z.string().min(8).max(50),
+    confirmPassword: z.string().min(7).max(50),
   })
-  .refine((data) => data.password === data.confirmPassword, {
-    message: "Passwords do not match",
-    path: ["confirmPassword"],
+  .superRefine(({password, confirmPassword}, ctx) => {
+    if(password !== confirmPassword) {
+       ctx.addIssue({
+        code: "custom",
+        path: ["confirmPassword"],
+        message: "Passwords do not match"
+
+       })
+    }
   })
 
+
 const RegisterPage = () => {
+
+  const form = useForm<z.infer<typeof registerFormSchema>>({
+    resolver: zodResolver(registerFormSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+      confirmPassword: "",
+    },
+  })
+
+  const onSubmit = (values: z.infer<typeof registerFormSchema>) => {
+  console.log(values);
+}
+
+
   return (
-    <div>RegisterPage</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        {/* Email field */}
+        <FormField 
+          control={form.control}
+          name="email"
+          
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter your email" {...field} type='email'/>
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+          )}/>
+
+          {/* Password field */}
+          <FormField 
+          control={form.control}
+          name="password"
+          
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Enter password" {...field} type='password'/>
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+          )}/>
+
+          {/* Matching Password field */}
+          <FormField 
+          control={form.control}
+          name="confirmPassword"
+          
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Confirm Password</FormLabel>
+              <FormControl>
+                <Input placeholder="Confirm password" {...field} type='password'/>
+              </FormControl>
+              <FormMessage/>
+            </FormItem>
+          )}/>
+
+          
+          <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
-export default RegisterPage
+
+export default RegisterPage;
